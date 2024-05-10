615c009896156e76c78201f3e470d21693a0d401
Tatyana Tkachenko <teperik24@YANDEX.RU>
2024-05-11 00:04:28 +0300

    итог+++(после 2го ревью)

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -5,53 +5,49 @@ from . import constants
 from .models import Category, Post
 
 
-def get_posts(post_objects, username=None):
-    all_published_post = post_objects.filter(
+def get_posts(post_objects):
+    return post_objects.filter(
         is_published=True,
         category__is_published=True,
         pub_date__lte=Now()
     )
-    if username is not None:
-        posts_by_username = post_objects.filter(
-            author=username
-        )
-        return all_published_post | posts_by_username
-    return all_published_post
 
 
 def index(request):
-    username = request.user
-    if request.user.is_anonymous:
-        username = None
-    template = 'blog/index.html'
     post_list = get_posts(
-        Post.objects, username=username
+        Post.objects
     ).all()[:constants.POSTS_LIMIT]
+    if request.user.is_anonymous is False:
+        post_list = post_list | Post.objects.filter(
+            author=request.user)
+    template = 'blog/index.html'
     context = {'post_list': post_list}
     return render(request, template, context)
 
 
 def post_detail(request, post_id):
-    username = request.user
-    if request.user.is_anonymous:
-        username = None
     template = 'blog/detail.html'
-    posts = get_object_or_404(
-        get_posts(Post.objects, username=username).all(), id=post_id)
+    if request.user.is_anonymous is False:
+        posts = get_object_or_404(
+            get_posts(Post.objects) | Post.objects.filter(
+                author=request.user), id=post_id)
+    else:
+        posts = get_object_or_404(
+            get_posts(Post.objects), id=post_id)
     context = {'post': posts}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
-    username = request.user
-    if request.user.is_anonymous:
-        username = None
-    template = 'blog/category.html'
     category = get_object_or_404(
         Category,
         slug=category_slug,
         is_published=True
     )
-    post_list = get_posts(category.posts, username).all()
+    post_list = get_posts(category.posts).all()
+    if request.user.is_anonymous is False:
+        post_list = post_list | category.posts.filter(
+            author=request.user)
+    template = 'blog/category.html'
     context = {'category': category, 'post_list': post_list}
     return render(request, template, context)
